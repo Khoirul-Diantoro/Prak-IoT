a08be6c23f976b9aa6fe0959011657a78d2b48fb
Dian of arc <83386562+Khoirul-Diantoro@users.noreply.github.com>
2021-09-19 19:01:10 +0700

    Update main.js

--- Tugas 4/main.js
+++ Tugas 4/main.js
@@ -7,10 +7,10 @@ function loop(){
 	var motion = digitalRead(0);
 	Serial.println(motion);
 	
-	if (motion == 1023){
-		digitalWrite(1, HIGH)
+	if (motion == 1023){ 
+		digitalWrite(1, HIGH) // jika terjadi gerakan dari cursor mouse maka akan menyalakan lampu.
 	}
 	else {
-		digitalWrite(1, LOW);
+		digitalWrite(1, LOW); // jika tidak terdapat gerakan maka lampu akan mati.
 	}
 }
