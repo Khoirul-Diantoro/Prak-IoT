6caa1afdeaddcb4294359540dd8b6c136e2a64fd
Dian of arc <83386562+Khoirul-Diantoro@users.noreply.github.com>
2021-09-19 14:31:12 +0700

    Update main.js

--- Tugas 4/main.js
+++ Tugas 4/main.js
@@ -1,1 +1,16 @@
+function setup(){
+	pinMode(0, INPUT);
+	pinMode(1, OUTPUT);
+}
 
+function loop(){
+	var motion = digitalRead(0);
+	Serial.println(motion);
+	
+	if (motion == 1023){
+		digitalWrite(1, HIGH)
+	}
+	else {
+		digitalWrite(1, LOW);
+	}
+}
