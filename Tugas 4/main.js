cb6906cb337a36f6f2c89fdb5b41c4d8af864719
Dian of arc <83386562+Khoirul-Diantoro@users.noreply.github.com>
2021-09-19 19:02:37 +0700

    Update main.js

--- Tugas 4/main.js
+++ Tugas 4/main.js
@@ -1,11 +1,11 @@
 function setup(){
 	pinMode(0, INPUT);
-	pinMode(1, OUTPUT);
+	pinMode(1, OUTPUT); // output bernilai HIGH dan LOW
 }
 
 function loop(){
-	var motion = digitalRead(0);
-	Serial.println(motion);
+	var motion = digitalRead(0); // untuk mendeklarasikan motion sebagai inputan.
+	Serial.println(motion); // mengeluarkan data motion.
 	
 	if (motion == 1023){ 
 		digitalWrite(1, HIGH) // jika terjadi gerakan dari cursor mouse maka akan menyalakan lampu.
